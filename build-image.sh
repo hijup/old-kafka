3b1e5097a5b487dace5cb1a902bc5d809ebf6706
aditya pratama <aditya@hijup.com>
2017-11-13 19:04:04 +0700

    Update values

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 echo "   ==> Build image"
-docker build -t asia.gcr.io/hijup-cloud/kafka:0.11.0.1 .
+docker build -t asia.gcr.io/hijup-cloud/kafka:0.11.0.0 .
 
 echo "   ==> Push image"
-docker push asia.gcr.io/hijup-cloud/kafka:0.11.0.1
+docker push asia.gcr.io/hijup-cloud/kafka:0.11.0.0
